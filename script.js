6fbf90ce3f378a2cf7bbda7fef33ffc35d8f28a1
khair2nc <91472790+khair2nc@users.noreply.github.com>
2026-05-24 20:47:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+// script.js
+
 const category = document.getElementById("category");
+
 const dynamicFields = document.getElementById("dynamicFields");
+
 const searchBtn = document.getElementById("searchBtn");
 
 const cardSection = document.getElementById("cardSection");
 
 const personName = document.getElementById("personName");
+
 const personType = document.getElementById("personType");
 
 const extraInfo1 = document.getElementById("extraInfo1");
+
 const extraInfo2 = document.getElementById("extraInfo2");
+
 const extraInfo3 = document.getElementById("extraInfo3");
+
 const extraInfo4 = document.getElementById("extraInfo4");
 
+const extraInfo5 = document.getElementById("extraInfo5");
 
-// Dynamic Input Fields
+const extraInfo6 = document.getElementById("extraInfo6");
+
+
+// Dynamic Input
+
 category.addEventListener("change", () => {
 
     let value = category.value;
@@ -23,160 +36,157 @@ category.addEventListener("change", () => {
         dynamicFields.innerHTML = `
 
             <div class="input-group">
+
                 <label>ইনডেক্স নং</label>
+
                 <input type="text" id="indexNo">
+
             </div>
 
             <div class="input-group">
+
                 <label>জন্মতারিখ</label>
+
                 <input type="date" id="birthDate">
+
             </div>
 
         `;
 
-    }
-
-    else if(value === "student"){
-
-        dynamicFields.innerHTML = `
-
-            <div class="input-group">
-                <label>শ্রেণি</label>
-                <input type="text" id="className">
-            </div>
-
-            <div class="input-group">
-                <label>রোল নং</label>
-                <input type="text" id="rollNo">
-            </div>
-
-        `;
-
-    }
-
-    else{
-        dynamicFields.innerHTML = "";
     }
 
 });
 
 
 // Dummy Data
+
 const users = {
 
     teacher:{
-        name:"রেবা পারভিন",
+
+        name:"মোঃ রহমান",
+
         position:"সহকারী শিক্ষক",
-        subject:"সমাজ বিজ্ঞান",
-        index:"1052040",
-        mobile:"01714562626",
-        blood:"AB+",
-        join:"05-01-2005",
-        expire:"11/01/2034"
+
+        index:"T-102",
+
+        subject:"বাংলা",
+
+        join:"01-01-2020",
+
+        expire:"31-12-2028",
+
+        mobile:"01700000000",
+
+        blood:"A+"
+
     },
 
     staff:{
-        name:"মোঃ করিম",
-        position:"অফিস সহকারী",
-        index:"S-205",
-        mobile:"01800000000",
-        blood:"B+",
-        join:"05-02-2021",
-        expire:"31-12-2028"
-    },
 
-    student:{
-        name:"সুমাইয়া আক্তার",
-        class:"দশম",
-        roll:"12",
-        session:"2026"
+        name:"মোঃ করিম",
+
+        position:"অফিস সহকারী",
+
+        index:"S-205",
+
+        subject:"অফিস",
+
+        join:"05-02-2021",
+
+        expire:"31-12-2028",
+
+        mobile:"01800000000",
+
+        blood:"B+"
+
     }
 
 };
 
 
-// Search Button
+// SEARCH
+
 searchBtn.addEventListener("click", () => {
 
     let value = category.value;
 
     if(value === ""){
+
         alert("ক্যাটাগরি নির্বাচন করুন");
+
         return;
+
     }
 
     cardSection.classList.remove("hidden");
 
-    // Teacher
+
+    // TEACHER
+
     if(value === "teacher"){
 
-        personName.innerText = users.teacher.name.toUpperCase();
+        personName.innerText = users.teacher.name;
 
         personType.innerText = users.teacher.position;
 
-        extraInfo1.innerText = "ইনডেক্স নং: " + users.teacher.index;
-        
-        extraInfo1.innerText = "বিষয়: " + users.teacher.index;
+        extraInfo1.innerText = users.teacher.index;
 
-        extraInfo2.innerText = "মোবাইল: " + users.teacher.mobile;
+        extraInfo2.innerText = users.teacher.subject;
 
-        extraInfo3.innerText = "রক্তের গ্রুপ: " + users.teacher.blood;
+        extraInfo3.innerText = users.teacher.join;
 
-        extraInfo4.innerText = "যোগদান: " + users.teacher.join;
-        
-        extraInfo1.innerText = "মেয়াদ উত্তীর্ণ: " + users.teacher.index;
+        extraInfo4.innerText = users.teacher.expire;
+
+        extraInfo5.innerText = users.teacher.mobile;
+
+        extraInfo6.innerText = users.teacher.blood;
 
     }
 
-    // Staff
+
+    // STAFF
+
     else if(value === "staff"){
 
-        personName.innerText = users.staff.name.toUpperCase();
+        personName.innerText = users.staff.name;
 
         personType.innerText = users.staff.position;
 
-        extraInfo1.innerText = "ইনডেক্স নং: " + users.staff.index;
+        extraInfo1.innerText = users.staff.index;
 
-        extraInfo2.innerText = "মোবাইল: " + users.staff.mobile;
+        extraInfo2.innerText = users.staff.subject;
 
-        extraInfo3.innerText = "রক্তের গ্রুপ: " + users.staff.blood;
+        extraInfo3.innerText = users.staff.join;
 
-        extraInfo4.innerText = "যোগদান: " + users.staff.join;
+        extraInfo4.innerText = users.staff.expire;
 
-    }
+        extraInfo5.innerText = users.staff.mobile;
 
-    // Student
-    else if(value === "student"){
-
-        personName.innerText = users.student.name.toUpperCase();
-
-        personType.innerText = "স্টুডেন্ট";
-
-        extraInfo1.innerText = "শ্রেণি: " + users.student.class;
-
-        extraInfo2.innerText = "রোল: " + users.student.roll;
-
-        extraInfo3.innerText = "শিক্ষাবর্ষ: " + users.student.session;
-
-        extraInfo4.innerText = "";
+        extraInfo6.innerText = users.staff.blood;
 
     }
 
 
-    // QR Code
+    // QR CODE
+
     document.getElementById("qrcode").innerHTML = "";
 
     new QRCode(document.getElementById("qrcode"),{
+
         text:"https://npcdacademy.edu.bd/profile",
-        width:100,
-        height:100
+
+        width:70,
+
+        height:70
+
     });
 
 });
 
 
+// PDF DOWNLOAD
 
-// PDF Download
 document.getElementById("downloadBtn").addEventListener("click", async () => {
 
     const { jsPDF } = window.jspdf;
@@ -184,20 +194,23 @@ document.getElementById("downloadBtn").addEventListener("click", async () => {
     const pdf = new jsPDF("p","mm","a4");
 
     const frontCard = document.getElementById("frontCard");
+
     const backCard = document.getElementById("backCard");
 
     const frontCanvas = await html2canvas(frontCard);
+
     const backCanvas = await html2canvas(backCard);
 
     const frontImg = frontCanvas.toDataURL("image/png");
+
     const backImg = backCanvas.toDataURL("image/png");
 
-    pdf.text("NPCD Academy ID Card", 70, 10);
+    pdf.text("NPCD Academy ID Card", 65, 10);
 
-    pdf.addImage(frontImg, "PNG", 15, 20, 80, 120);
+    pdf.addImage(frontImg, "PNG", 20, 20, 54, 86);
 
-    pdf.addImage(backImg, "PNG", 110, 20, 80, 120);
+    pdf.addImage(backImg, "PNG", 100, 20, 54, 86);
 
-    pdf.save("id-card.pdf");
+    pdf.save("npcd-id-card.pdf");
 
 });
